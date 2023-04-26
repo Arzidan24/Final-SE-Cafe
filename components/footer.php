c135d46afa8cf62f5d7b4a374f33e1fa3d4cf746
Arzidan Fadilla <92257984+Arzidan24@users.noreply.github.com>
2023-04-26 17:14:18 +0700

    first commit

---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:shaikhanas@gmail.com">cafe@gmail.com</a>
+         <a href="mailto:anasbhai@gmail.com">cafe@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>00:07am to 00:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">cikarang, bekasi - 17550</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+         <a href="tel:1112223333">111-222-3333</a>
+      </div>
+
+   </section>
+
+   <div class="credit">Thank you for coming to our website. <span>Don't forget to come back.</span> Many thanks!</div>
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
