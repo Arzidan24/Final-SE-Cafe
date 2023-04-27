f524b78f82296db4cad12c40228d4cf42106d81e
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2023-04-27 11:33:00 +0700

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -5,14 +5,14 @@
       <div class="box">
          <img src="images/email-icon.png" alt="">
          <h3>our email</h3>
-         <a href="mailto:shaikhanas@gmail.com">cafe@gmail.com</a>
-         <a href="mailto:anasbhai@gmail.com">cafe@gmail.com</a>
+         <a href="mailto:cafe@gmail.com">cafe@gmail.com</a>
+         <a href="mailto:cafe@gmail.com">cafe@gmail.com</a>
       </div>
 
       <div class="box">
          <img src="images/clock-icon.png" alt="">
          <h3>opening hours</h3>
-         <p>00:07am to 00:10pm</p>
+         <p>7am to 10pm</p>
       </div>
 
       <div class="box">
